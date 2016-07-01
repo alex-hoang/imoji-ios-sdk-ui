ef9a408d3f77b02c829e8094597a54a7d4ed4d84
nkhoshini <nimakhoshini@gmail.com>
2016-06-30 17:54:14 -0700

    Adds an iOS 10 Messages Framework example

---
+++ Examples/MessagesSample/MessagesExtension/MessagesViewController.h
@@ -0,0 +1,13 @@
+//
+//  MessagesViewController.h
+//  MessagesExtension
+//
+//  Created by Nima on 6/30/16.
+//  Copyright © 2016 Imoji. All rights reserved.
+//
+
+#import <Messages/Messages.h>
+
+@interface MessagesViewController : MSMessagesAppViewController
+
+@end
